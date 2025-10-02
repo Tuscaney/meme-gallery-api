e251542feedb1b206944ecfb8fb5ba0a61c26c58
TutCode222 <tuddycarraway@gmail.com>
2025-10-02 17:34:18 -0500

    feat(mvc): wire routes to controller

---
+++ routes/memeRoutes.js
@@ -0,0 +1,13 @@
+// routes/memeRoutes.js
+import express from "express";
+import { getMemes, getMemeById, createMeme, updateMeme, deleteMeme } from "../controllers/memeController.js";
+
+const router = express.Router();
+
+router.get("/", getMemes);
+router.get("/:id", getMemeById);
+router.post("/", createMeme);
+router.put("/:id", updateMeme);
+router.delete("/:id", deleteMeme);
+
+export default router;
